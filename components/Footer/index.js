3eacd98e6efad627c9b04c128ab95be9a530ae58
Finbar Maginn <finbar.maginn@dixonscarphone.com>
2022-10-09 17:50:10 +0100

    CSS reorganising and playing around

---
+++ components/Footer/index.js
@@ -0,0 +1,13 @@
+import Nav from "../Nav";
+import styles from './index.module.css'
+
+export function Footer() {
+  return (
+    <div>
+      <footer>
+        Finbar Maginn 2022
+        <Nav />
+      </footer>
+    </div>
+  )
+}
