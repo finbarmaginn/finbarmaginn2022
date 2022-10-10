c579510f3f1aded48ceb52b07b835ca4707348cf
Finbar Maginn <finbar.maginn@dixonscarphone.com>
2022-10-10 19:28:23 +0100

    add footer modular scss

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,7 +1,9 @@
+import styles from './index.module.scss'
+
 export function Footer() {
   return (
     <div>
-      <footer>
+      <footer className={styles.foot}>
         <p>Finbar Maginn 2022</p>
       </footer>
     </div>
